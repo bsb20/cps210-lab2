e03656dd1d153634888098d1698ab915b24d5ae9
Ian Rapoport <ian.h.rapoport@gmail.com>
2012-10-29 21:28:43 -0400

    Added comments, need to comments 1-2 more classes then will submit

--- src/lab2/Building.java
+++ src/lab2/Building.java
@@ -3,7 +3,11 @@ package lab2;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+ * Each rider requests a floor by calling await up/down on the Building
+ * The building starts/stops elevators and helps coordinate the elevators
+ * with the Riders
+ */
 public class Building
 {
 	
@@ -16,18 +20,28 @@ public class Building
 		}
 
 	}
-
+	
+	/*
+	 * Returns the 
+	 */
     private Elevator findElevator(int floor, boolean upwards) {
         return myController.findElevator(floor, upwards);
     }
-
+	
+	/*
+	 * A call to await finds the elevator that will arrive for the Rider, and 
+	 * returns an instance of the elevator to the calling Rider. If the elevator
+	 * is full or headed in the wrong direction, the Building's monitor uses a 
+	 * wait() CV to block the calling thread until the doors close and the floor 
+	 * request can be made again
+	 */
     private Elevator await(int floor, boolean upwards) {
         Elevator elevator = findElevator(floor,upwards);
         System.out.println("E" + elevator.getElevatorId() + " called");
         synchronized (this) {
             while (elevator.isFull()) {
                 try {
-                    wait();
+                    wait(); //if elevator full, block rider thread until doors close and new request can be made
                 }
                 catch (InterruptedException e) {}
             }
@@ -38,30 +52,42 @@ public class Building
             synchronized (this) {
                 while (elevator.isOpen()) {
                     try {
-                        wait();
+                        wait(); //if elevator in wrong direction, block rider thread until doors close and new request can be made
                     }
                     catch (InterruptedException e) {}
                 }
             }
             elevator.requestFloor(floor, upwards);
         }
-		return elevator;
+		return elevator; //elevator is in correct direction and has capacity for another rider
     }
-
+	
+	/*
+	 * Returns a reference to the arriving elevator when a Rider requests a ride down
+	 */ 
 	public Elevator awaitUp(int floor) {
         return await(floor, true);
 	}
 
+	/*
+	 * Returns a reference to the arriving elevator when a Rider requests a ride up
+	 */ 
 	public Elevator awaitDown(int floor) {
         return await(floor, false);
 	}
 
+	/*
+	 * Starts all Elevator threads in the Building
+	 */
 	public void startElevators(){
 		for(Elevator e: myController.getElevators()){
 			e.start();
 		}
 	}
 
+	/*
+	 * Stops all Elevator threads in the Building
+	 */
 	public void stopElevators(){
 		for(Elevator e: myController.getElevators()){
 			e.interrupt();
--- src/lab2/part2/RiderThread.java
+++ src/lab2/part2/RiderThread.java
@@ -1,9 +1,11 @@
 package lab2.part2;
-
 import lab2.Building;
 import lab2.Elevator;
 
-
+/*
+ * Thread that executes requests from the elevator Riders
+ * Each loop of thread processes a new Rider request
+ */
 public class RiderThread extends Thread
 {
     private int myId;
@@ -14,7 +16,14 @@ public class RiderThread extends Thread
         myBuilding = building;
         myId = id;
     }
-
+	
+	/*
+	 * Thread loops, each time receiving the riderID, start/end floors of the Rider
+	 * Rider awaits the elevator arrival, attempts to enter, and if successful, requests
+	 * its floor, and exits the elevator on the appropriate floor
+	 *
+	 * If the rider encounters a full elevator, it waits again for a elevator to arrive 
+	 */
     @Override
     public void run() {
         int[] rider = Main.nextRider();
@@ -24,11 +33,11 @@ public class RiderThread extends Thread
             int endFloor = rider[2];
 
             Elevator elevator;
-            while (true) {
+            while (true) {    //Call to awaitup/down blocks the thread on an EventBarrier until the elevator opens on its floor
                 if (startFloor < endFloor) {
                     System.out.println(String.format("T%d: R%d pushes U%d",
                                                      myId, riderId, startFloor));
-                    elevator = myBuilding.awaitUp(startFloor);
+                    elevator = myBuilding.awaitUp(startFloor); 
                 }
                 else {
                     System.out.println(String.format("T%d: R%d pushes D%d",
@@ -44,13 +53,13 @@ public class RiderThread extends Thread
                                              myId, riderId,
                                              elevator.getElevatorId(),
                                              endFloor));
-            elevator.requestFloor(endFloor);
+            elevator.requestFloor(endFloor);  //floor request blocks thread until the elevator doors open and the Rider exits
 
             System.out.println(String.format("T%d: R%d exits E%d on F%d",
                                              myId, riderId,
                                              elevator.getElevatorId(),
                                              endFloor));
-            elevator.exit();
+            elevator.exit();      
 
             rider = Main.nextRider();
         }
